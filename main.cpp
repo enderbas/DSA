50632f70ab654828a46a3c84bab98cb5ae65d7fa
Engin Deniz Erbas <engin.erbas@sparsetechnology.com>
2021-11-28 21:53:47 +0300

    new stack algorithm changes applied to main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-	Stack <std::string> stAlgo;
+	Stack<std::string> stAlgo(5);
 	std::cout<< "is empty "<<stAlgo.isEmpty() << " is full "<<stAlgo.isFull() <<std::endl;
 	stAlgo.push( "word1");
 	stAlgo.push( "word2");
@@ -17,8 +17,9 @@ int main()
 	stAlgo.pop();
 	std::cout<<stAlgo.peek()<<std::endl;
 	stAlgo.pop();
-	Stack <int> stAlgoi;
-	std::cout<< "is empty main "<<stAlgoi.isEmpty() << " is full "<<stAlgoi.isFull() <<std::endl;
+	Stack<int> stAlgoi(20);
+	std::cout << "is empty main " << stAlgoi.isEmpty() << " is full "
+			  << stAlgoi.isFull() << std::endl;
 	stAlgoi.push(1);
 	std::cout<<stAlgoi.peek()<<std::endl;
 	stAlgoi.pop();
